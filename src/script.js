da78815ffd59d5327776a17a610bb8d36207580a
syntask <161401651+syntask@users.noreply.github.com>
2024-05-28 22:26:56 -0500

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById('current-year').textContent = currentYear;
+});
